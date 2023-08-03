f21aa32151e1d4d14347c6b3b80ca5b16260274c
Emm4nuell <eduardo.eesf@gmail.om>
2023-08-02 23:32:36 -0300

    Adicionado formação e certificados na página Portfólio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ParentDataComponent } from './components/parent-data/parent-data.component';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { IfRenderComponent } from './components/if-render/if-render.component';
+import { EventosComponent } from './components/eventos/eventos.component';
+import { ListRenderComponent } from './components/list-render/list-render.component';
+import { HeaderComponent } from './components/template/header/header.component';
+import { NavComponent } from './components/template/nav/nav.component';
+import { FooterComponent } from './components/template/footer/footer.component';
+import { HomeComponent } from './views/home/home.component';
+import { ContentComponent } from './components/template/content/content.component';
+import { SobreComponent } from './views/sobre/sobre.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParentDataComponent,
+    FirstComponentComponent,
+    IfRenderComponent,
+    EventosComponent,
+    ListRenderComponent,
+    HeaderComponent,
+    NavComponent,
+    FooterComponent,
+    HomeComponent,
+    ContentComponent,
+    SobreComponent
   ],
   imports: [
     BrowserModule,
