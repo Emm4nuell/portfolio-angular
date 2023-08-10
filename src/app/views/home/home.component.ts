75e90781e6c2f5168895fd7e33690c594da9167b
Emm4nuell <eduardo.eesf@gmail.om>
2023-08-10 11:09:43 -0300

    Modificações no css responsivo

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
-  fala(): void{
-    let x = document.querySelector(".falacomigo");
-    console.log(x?.getBoundingClientRect);
+  nome : String = "";
+
+  constructor(){}
+
+  ngOnInit(): void {}
+
+  obterValor(nome : String){
+    this.nome = nome;
   }
-
 }
